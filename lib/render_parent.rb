5e85b03818d6b9406523f9aecf5f9eaa454a8f1b
MayamaTakeshi <mayamatakeshi@gmail.com>
2017-09-10 09:05:51 +0900

    Added support for Rails 5

--- lib/render_parent.rb
+++ lib/render_parent.rb
@@ -1,4 +1,6 @@
-if Rails::VERSION::MAJOR >= 3
+if Rails::VERSION::MAJOR >= 5
+  require 'render_parent/rails5'
+elsif Rails::VERSION::MAJOR >= 3
   require 'render_parent/rails3'
 elsif Rails::VERSION::MAJOR == 2
   require 'render_parent/rails2'
